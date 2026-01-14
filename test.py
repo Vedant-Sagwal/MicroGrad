69a202e0835eff13bb08e7793c7975f032fb5164
Vedant-Sagwal <vedantsagwal@gmail.com>
2026-01-14 19:52:21 +0530

    comparing micrograd with torch autograd

---
+++ test.py
@@ -0,0 +1,31 @@
+#test torch autograd with micrograd
+
+import torch
+from engine import Value
+
+def test_sanity_check():
+
+    x = Value(-4.0)
+    z = 2 * x + 2 + x
+    q = z.tanh() + z * x
+    h = (z * z).tanh()
+    y = h + q + q * x
+    y.backward()
+    xmg, ymg = x, y
+
+    x = torch.Tensor([-4.0]).double()
+    x.requires_grad = True
+    z = 2 * x + 2 + x
+    q = z.tanh() + z * x
+    h = (z * z).tanh()
+    y = h + q + q * x
+    y.backward()
+    xpt, ypt = x, y
+
+    # forward pass went well
+    assert ymg.data == ypt.data.item()
+    # backward pass went well
+    assert xmg.grad == xpt.grad.item()
+
+
+test_sanity_check()
